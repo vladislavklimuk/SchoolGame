4e5d08586330bd4a89d3dc769d037902ef73414a
Eris <erisihack@gmail.com>
2016-04-27 23:14:19 +0300

    Ohhh... cookies ..

--- question/lesson.js
+++ question/lesson.js
@@ -1,1 +1,29 @@
 var globresult1kom; 
+var ferstCom=0;
+var secondCom=0;
+var ochered;
+	function ansTrue () {
+		console.log(ochered);
+	if (ochered==2) {
+
+
+
+
+		ochered=1;
+		
+		document.location.href='../index.html';
+		console.log(ochered);
+	} else{
+
+
+
+		
+		document.location.href='1q20ex.html';
+		ochered=2;
+		console.log(ochered);
+	}
+
+
+
+	
+}
