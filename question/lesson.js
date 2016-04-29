a11d62b25166cec8f69d3acbbc10f33792a72a57
Eris <erisihack@gmail.com>
2016-04-30 00:14:12 +0300

    icon reload

--- question/lesson.js
+++ question/lesson.js
@@ -1,29 +1,88 @@
 var globresult1kom; 
 var ferstCom=0;
 var secondCom=0;
-var ochered;
-	function ansTrue () {
-		console.log(ochered);
-	if (ochered==2) {
+var ochered=2;
+var exp=10;
+    //Функция отображения PopUp
+    function PopUpShow(){
+        $("#popup1").show();
+    }
+    //Функция скрытия PopUp
+    function PopUpHide(){
+        $("#popup1").hide();
+    }
+    
 
 
 
 
-		ochered=1;
+
+
+    function ball(){
+	if (localStorage.getItem('ochered')==2) {
 		
-		document.location.href='../index.html';
-		console.log(ochered);
-	} else{
-
-
-
+		localStorage.setItem('ochered', '1');
 		
-		document.location.href='1q20ex.html';
-		ochered=2;
-		console.log(ochered);
+		if(typeof(Storage) !== "undefined") {
+			if (localStorage.EXP2team) {
+    		localStorage.EXP2team = Number(localStorage.EXP2team) + exp;
+			} else {
+    		localStorage.EXP2team = exp;
+			}}
+
+	  	document.location.href='../index.html';
+
+
+
 	}
 
 
 
-	
-}
+
+			
+
+	else {
+		if(typeof(Storage) !== "undefined") {
+			if (localStorage.EXP1team) {
+    		localStorage.EXP1team = Number(localStorage.EXP1team) + exp;
+			}else{
+    		localStorage.EXP1team = exp;}}
+	localStorage.setItem('ochered', '2');
+	document.location.href='../index.html';
+	console.log(ochered);
+
+		}
+
+
+	}	
+
+
+
+
+
+	function cler(){
+		localStorage.clear();
+		location.reload();
+	}
+
+
+
+	function miss () {
+		if (localStorage.getItem('ochered')==2) {
+		
+		localStorage.setItem('ochered', '1');
+		
+		
+	  document.location.href='../index.html';}
+
+
+
+
+			
+
+	else {
+		localStorage.setItem('ochered', '2');
+	document.location.href='../index.html';
+	console.log(ochered);}
+
+	}
